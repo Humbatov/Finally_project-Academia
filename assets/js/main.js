6588c8ba48ec064174126a10b20c6bfa1060fbbd
Tofig <tofikshamedov@gmail.com>
2016-09-10 22:02:31 +0400

    home page

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+//read more button
+$('.moreButton').click(function(event) {
+	$(this).css('display','none');
+	$(this).siblings('.paperText').css('height', '200px');
+    var fullHeight = $(this).siblings('.paperText').height();
+	$(this).siblings('.paperText').animate({height: fullHeight}, 500);
+
+});
+
+//active class
+$(document).ready(function () {
+    $('.ulBtn li a').click(function () {
+          $('.ulBtn li').removeClass('active');
+        $(this).parent().addClass('active');
+           return true;
+    });
+ });
+//menu
+$(document).ready(function(){
+	var touch = $('#touch-menu');
+    var menu = $('.navig');
+ 
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function(){
+        var wid = $(window).width();
+        if(wid > 760 && menu.is(':hidden')) {
+            menu.css('display','none');
+        }
+        if(wid < 760 ) {
+            $('#jobBoard').css('display','none');
+        }
+    });
+});
+//fixed jobBoard
+$(document).ready(function(){
+
+        $(window).scroll(function(){
+    	var Top = $(this).scrollTop();
+		if ( Top >= 100 ) {
+			$("#jobBoard").addClass('fixed')
+		};
+
+        if ( Top < 100 ) {
+        	$("#jobBoard").removeClass('fixed')
+        };
+
+    })
+})
