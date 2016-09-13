28b8286f030fa33e18528a681f4f168ec39ca9fe
Humbatov <chingiz.h@code.edu.az>
2016-09-13 21:37:26 +0400

    Search pages were added

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+//read more button
+
+$('.moreButton').click(function(event) {
+    $(this).css('display', 'none');
+    $(this).siblings('.paperText').css('height', '100%');
+
+});
+
+//active class
+$(document).ready(function() {
+    $('.ulBtn li a').click(function() {
+        $('.ulBtn li').removeClass('active');
+        $(this).parent().addClass('active');
+        return true;
+    });
+});
+//menu
+$(document).ready(function() {
+    var touch = $('#touch-menu');
+    var menu = $('.navig');
+
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function() {
+        var wid = $(window).width();
+        if (wid > 760 && menu.is(':hidden')) {
+            menu.css('display', 'none');
+
+        }
+
+    });
+});
+//fixed jobBoard
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        var Top = $(this).scrollTop();
+        if (Top >= 100) {
+            $("#jobBoard").addClass('fixed')
+        };
+
+        if (Top < 100) {
+            $("#jobBoard").removeClass('fixed')
+        };
+
+    })
+})
